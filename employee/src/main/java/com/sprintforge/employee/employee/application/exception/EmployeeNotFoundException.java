9d87b69b792b87497c67eb6798544ec2fa509705
Mariano Camposeco <mariano1941@outlook.es>
2025-12-11 15:04:06 -0600

    fix: change structure in exceptions

--- employee/src/main/java/com/sprintforge/employee/employee/application/exception/EmployeeNotFoundException.java
+++ employee/src/main/java/com/sprintforge/employee/employee/application/exception/EmployeeNotFoundException.java
@@ -1,7 +1,28 @@
 package com.sprintforge.employee.employee.application.exception;
 
-public class EmployeeNotFoundException extends RuntimeException {
-    public EmployeeNotFoundException(String type, String value) {
-        super("Empleado con " + type + " " + value + " no encontrado.");
+import com.sprintforge.employee.common.application.exception.EntityNotFoundException;
+
+import java.util.UUID;
+
+public class EmployeeNotFoundException extends EntityNotFoundException {
+
+    private EmployeeNotFoundException(String field, String value) {
+        super(String.format("No se encontró ningún empleado con %s \"%s\".", field, value));
+    }
+
+    public static EmployeeNotFoundException byId(UUID id) {
+        return new EmployeeNotFoundException("el identificador", id.toString());
+    }
+
+    public static EmployeeNotFoundException byCui(String cui) {
+        return new EmployeeNotFoundException("el CUI", cui);
+    }
+
+    public static EmployeeNotFoundException byEmail(String email) {
+        return new EmployeeNotFoundException("el correo electrónico", email);
+    }
+
+    public static EmployeeNotFoundException byPhone(String phoneNumber) {
+        return new EmployeeNotFoundException("el número de teléfono", phoneNumber);
     }
 }
