218db8a062d755b34aefef8d7b2cc30df2b9d0dc
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-17 19:47:10 -0600

    feat: add employee salary increase implementation with all exceptions and command modifications

--- employee/src/main/java/com/sprintforge/employee/employee/application/port/in/command/IncreaseEmployeeSalaryCommand.java
+++ employee/src/main/java/com/sprintforge/employee/employee/application/port/in/command/IncreaseEmployeeSalaryCommand.java
@@ -2,10 +2,9 @@ package com.sprintforge.employee.employee.application.port.in.command;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.UUID;
 
 public record IncreaseEmployeeSalaryCommand(
-    UUID employeeId,
+    String cui,
     BigDecimal increaseAmount,
     LocalDate date,
     String notes
--- employee/src/main/java/com/sprintforge/employee/employee/application/port/out/persistence/FindActiveEmploymentByEmployee.java
+++ employee/src/main/java/com/sprintforge/employee/employee/application/port/out/persistence/FindActiveEmploymentByEmployee.java
@@ -1,8 +1,10 @@
 package com.sprintforge.employee.employee.application.port.out.persistence;
 
+import java.util.Optional;
+
 import com.sprintforge.employee.employee.domain.Employee;
 import com.sprintforge.employee.employee.domain.EmploymentHistory;
 
 public interface FindActiveEmploymentByEmployee {
-    EmploymentHistory findActiveByEmployee(Employee employee);
+    Optional<EmploymentHistory> findActiveByEmployee(Employee employee);
 }
