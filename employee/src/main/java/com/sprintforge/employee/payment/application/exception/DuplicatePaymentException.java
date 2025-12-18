c2cf7a0aa2a7cb3db189a4ac9e91f45550db9fc3
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-18 01:03:06 -0600

    refactor: change type of the date param

--- employee/src/main/java/com/sprintforge/employee/payment/application/exception/DuplicatePaymentException.java
+++ employee/src/main/java/com/sprintforge/employee/payment/application/exception/DuplicatePaymentException.java
@@ -1,9 +1,11 @@
 package com.sprintforge.employee.payment.application.exception;
 
+import java.time.LocalDate;
+
 import com.sprintforge.common.application.exception.DuplicateEntityException;
 
 public class DuplicatePaymentException extends DuplicateEntityException {
-    public DuplicatePaymentException(String cui, String date) {
+    public DuplicatePaymentException(String cui, LocalDate date) {
         super("Pago duplicado para el empleado con CUI " + cui + " en la fecha " + date);
     }
     
--- employee/src/main/java/com/sprintforge/employee/payment/application/exception/PaymentDateBeforeLastException.java
+++ employee/src/main/java/com/sprintforge/employee/payment/application/exception/PaymentDateBeforeLastException.java
@@ -1,9 +1,11 @@
 package com.sprintforge.employee.payment.application.exception;
 
+import java.time.LocalDate;
+
 import com.sprintforge.common.application.exception.BusinessException;
 
 public class PaymentDateBeforeLastException extends BusinessException {
-    public PaymentDateBeforeLastException(String cui, String lastDate) {
+    public PaymentDateBeforeLastException(String cui, LocalDate lastDate) {
         super("La fecha del pago no puede ser anterior a la última fecha de pago " + lastDate + " para el empleado con CUI " + cui);
     }
 }
