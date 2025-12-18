c50ac15df7a35014416da7647efe59c4b84f9ed4
Manuel Rojas <manuelrojas202030799@cunoc.edu.gt>
2025-12-18 01:12:22 -0600

    delete: innecesary employee param in pay employee command

--- employee/src/main/java/com/sprintforge/employee/payment/application/port/in/PayEmployeeCommand.java
+++ employee/src/main/java/com/sprintforge/employee/payment/application/port/in/PayEmployeeCommand.java
@@ -3,12 +3,9 @@ package com.sprintforge.employee.payment.application.port.in;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import com.sprintforge.employee.employee.domain.Employee;
-
 public record PayEmployeeCommand(
     String cui,
     LocalDate date,
-    Employee employee,
     BigDecimal bonus,
     BigDecimal deduction,
     String notes
